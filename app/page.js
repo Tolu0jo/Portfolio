8788cdab06a2d8a873a5a735b683aee64e806b4c
Teetony <ojotolulope@gmail.com>
2023-05-29 22:25:12 +0100

    profilepage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import {BsFillMoonStarsFill} from "react-icons/bs"
-import {AiFillTwitterCircle,AiFillLinkedin,AiFillInstagram} from "react-icons/ai"
+import {AiFillTwitterCircle,AiFillLinkedin,AiFillInstagram,AiFillGithub} from "react-icons/ai"
 import Image from "next/image"
-import tolu from "../public/tolu.jpeg"
+import tolu from "../public/tolu.png"
 export default function Home() {
   return (
     <main className="bg-white px-10">
     <section className="min-h-screen">
-      <nav className="py-10 mb-12 flex justify-between">
+      <nav className="py-7 mb-10 flex justify-between">
         <h1 className="text-xl font-burtons">
 
           Tolulope Ojo
@@ -16,23 +16,29 @@ export default function Home() {
           <li><a className ="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"href="#">Resume</a></li>
         </ul>
       </nav>
-      <div className="text-center p-10">
+      <div className="text-center p-6">
         <h2 className="text-5xl py-2 text-teal-600 font-small">Tolulope Ojo</h2>
         <h3 className="text-2xl py-2">Full Stack Software Developer</h3>
         <p className="text-md py-5 leading-8 text-gray-800">
           Software Developer with expertise
-          in Nodejs, React, Nextjs. Join me down below 
+          in Nodejs, React, CI/CD, Microservices. Join me down below 
           and let's keep cracking.
         </p>
       </div>
-      <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+      <div className="text-5xl flex justify-center gap-16 py-1 text-gray-600">
+      <AiFillGithub/>
       <AiFillLinkedin/>
       <AiFillTwitterCircle/>
       <AiFillInstagram/>
       </div>
-      <div>
-        <Image/>
+      <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-10 overflow-hidden">
+        <Image src={tolu} layout="fill" objectFit="cover"/>
       </div>
+    </section>
+    <section>
+      <h3>
+        
+      </h3>
     </section>
     </main>
   )
